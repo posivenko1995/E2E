ef91bf85622fb274ea6e9dd9f64fa11d5aff8f36
posivenko1995 <posivenko.andrey@mail.ru>
2024-09-12 10:18:30 +0500

    Add files via upload

---
+++ apiGitHub/config.py
@@ -0,0 +1,7 @@
+
+
+token = {''}# example token = {'Authorization': 'bearer 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'}
+usernameGitHub = ""# example usernameGitHub = "usermanegithub"
+namerepo = ""#  example namerepo = "namerepositories"
+
+
